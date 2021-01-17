9b75a9a3b43625224fca16b9a5cb4fbce191c9bb
Robertus06 <06robertus06@gmail.com>
2021-01-18 00:15:20 +0100

    Login terminado

--- src/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    
-    $.ajax({
-        url:"https://www.googleapis.com/books/v1/volumes?q=intitle:javascript"
-    }).done(function(data) {
-        console.log(data)
-    });
-});
