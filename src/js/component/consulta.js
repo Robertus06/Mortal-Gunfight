0fdccd48a2bf96eef42cb158f483672d282e8578
Robertus06 <06robertus06@gmail.com>
2021-01-18 21:27:02 +0100

    Mostrar jugadores conectados

--- src/js/component/consulta.js
+++ src/js/component/consulta.js
@@ -18,6 +18,33 @@ export default class Consulta{
             }).done(function(data) {
                 console.log(data);
                 this.jugadores = data;
+
+                var numJugadores = document.getElementById("contador");
+                numJugadores.innerHTML = "(" + this.jugadores.length + ")"
+
+                var element = document.getElementById("tablaJugadores");
+                while (element.firstChild) {
+                    element.removeChild(element.firstChild);
+                }
+
+                for (var i = 0; i < this.jugadores.length; i++){
+                    var newTr = document.createElement("tr");
+                    newTr.setAttribute("id", "destruir");
+                    var newContent = document.createElement("td")
+                    newContent.setAttribute("class", "jugador");
+                    var newH3 = document.createElement("h3")
+                    newH3.setAttribute("id", i+1);
+
+                    newContent.appendChild(newH3);
+                    newTr.appendChild(newContent);
+                    
+                    var tabla = document.getElementById("tablaJugadores");
+                    tabla.appendChild(newTr);
+    
+                    var newUser = document.getElementById(i+1);
+                    newUser.innerHTML = "" + this.jugadores[i];
+                }
+
             }.bind(this));
         }.bind(this));
     }
